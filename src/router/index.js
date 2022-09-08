8a721b03be5c4a93ae8b40bc94016327b2bee6bc
cherry <1044369926@qq.com>
2022-09-08 10:38:19 +0800

    feat:结算

--- src/router/index.js
+++ src/router/index.js
@@ -7,65 +7,70 @@
  * @LastEditors: cherry hong
  * @LastEditTime: 2022-08-05 15:19:18
  */
-import { createRouter, createWebHashHistory } from 'vue-router'
-import store from  "@/store"
-const Layout = () => import('@/views/Layout')
+import { createRouter, createWebHashHistory } from "vue-router";
+import store from "@/store";
+const Layout = () => import("@/views/Layout");
 const routes = [
   {
-    path: '/',
+    path: "/",
     component: Layout,
     children: [
       {
-        path: '/',
-        component: () => import('@/views/home/index')
+        path: "/",
+        component: () => import("@/views/home/index"),
       },
       {
-        path: '/category/:id',
-        component: () => import('@/views/category/index')
+        path: "/category/:id",
+        component: () => import("@/views/category/index"),
       },
       {
-        path: '/category/sub/:id',
-        component: () => import('@/views/category/sub')
+        path: "/category/sub/:id",
+        component: () => import("@/views/category/sub"),
       },
       {
-        path: '/product/:id',
-        component: () => import ('@/views/goods/index')
+        path: "/product/:id",
+        component: () => import("@/views/goods/index"),
       },
       {
-        path: '/cart',
-        component: () => import ('@/views/cart/index')
+        path: "/cart",
+        component: () => import("@/views/cart/index"),
       },
-     
-    ]
+      {
+        path: "/member/checkout",
+        component:() => import('@/views/member/pay/checkout')
+      },
+    ],
   },
   {
-    path: '/login',
-    component: () => import('@/views/login/index')
+    path: "/login",
+    component: () => import("@/views/login/index"),
   },
   {
-    path:'/i18n',
-    component: () => import('@/views/i18n/index')
-  }
-]
+    path: "/i18n",
+    component: () => import("@/views/i18n/index"),
+  },
+];
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
   scrollBehavior() {
     return {
-      left:0,
-      top:0
-
-    }
+      left: 0,
+      top: 0,
+    };
+  },
+});
+router.beforeEach((to, from, next) => {
+  const token = store.state.user?.profile?.token;
+  if (to.path.startsWith("/member/" && !token)) {
+    next({
+      path: "login",
+      query: {
+        redirectUrl: to.fullPath,
+      },
+    });
   }
-})
-router.beforeEach((to,from,next)=>{
-  const token = store.state.user?.profile?.token
-  if(to.path.startsWith('/member/'&&!token)){
-    next({path:'login',query:{
-      redirectUrl:to.fullPath
-    }})
-  }
-  next()
-})
-export default router
+  next();
+});
+export default router;
